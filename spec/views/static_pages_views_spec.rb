0b7329194fcd7a80cfaf73636ab9fd1878291054
Aspen Swanson <chris.b.swan@gmail.com>
2017-03-24 10:18:34 -0700

    Add testing for views and dynamic titles

---
+++ spec/views/static_pages_views_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'static_pages/_.html.erb', type: :view do
+  describe 'home.html.erb' do
+    it 'displays proper title' do
+      visit static_pages_home_url
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+  end
+
+  describe 'about.html.erb' do
+    it 'displays proper title' do
+      visit static_pages_about_url
+      expect(page).to have_title('About | Ruby on Rails Tutorial Sample App')
+    end
+  end
+
+  describe 'contact.html.erb' do
+    it 'displays proper title' do
+      visit static_pages_contact_url
+      expect(page).to have_title('Contact | Ruby on Rails Tutorial Sample App')
+    end
+  end
+
+  describe 'help.html.erb' do
+    it 'displays proper title' do
+      visit static_pages_help_url
+      expect(page).to have_title('Help | Ruby on Rails Tutorial Sample App')
+    end
+  end
+end
