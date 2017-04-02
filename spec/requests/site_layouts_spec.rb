b5f044b5d655d33bc8936da949028f87ad6d9787
Aspen Swanson <munin@gmx.com>
2017-04-02 11:33:38 -0700

    Removed default test suites, added user signup page and routes

--- spec/requests/site_layouts_spec.rb
+++ spec/requests/site_layouts_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-describe 'static_pages/home', type: :request do
+describe 'site layout', type: :request do
   describe 'GET root_path' do
-    subject { get root_path }
-    it { is_expected.to eq 200 }
-    it { is_expected.to render_template(:home) }
+    it 'renders the home template' do
+      expect(get root_path).to render_template(:home)
+    end
 
     describe 'home links and values' do
       before { visit home_path }
@@ -14,58 +14,69 @@ describe 'static_pages/home', type: :request do
         expect(page).to have_link('Help', href: help_path)
         expect(page).to have_link('About', href: about_path)
         expect(page).to have_link('Contact', href: contact_path)
+        expect(page).to have_link('Sign up now!', href: signup_path)
+      end
+    end
+  end
+
+  describe 'site navigation for' do
+    describe 'root' do
+      before { visit root_path }
+      it 'returns status 200' do
+        expect(page).to have_http_status '200'
+      end
+      it 'has proper title' do
+        expect(page).to have_title(full_title)
       end
     end
 
-    describe 'site navigation for' do
-      describe 'root' do
-        before { visit root_path }
-        it 'returns status 200' do
-          expect(page).to have_http_status '200'
-        end
-        it 'has proper title' do
-          expect(page).to have_title(full_title)
-        end
+    describe 'home' do
+      before { visit home_path }
+      it 'returns status 200' do
+        expect(page).to have_http_status '200'
       end
-
-      describe 'home' do
-        before { visit home_path }
-        it 'returns status 200' do
-          expect(page).to have_http_status '200'
-        end
-        it 'has proper title' do
-          expect(page).to have_title(full_title)
-        end
+      it 'has proper title' do
+        expect(page).to have_title(full_title)
       end
+    end
 
-      describe 'Contact' do
-        before { visit contact_path }
-        it 'returns status 200' do
-          expect(page).to have_http_status '200'
-        end
-        it 'has proper title' do
-          expect(page).to have_title(full_title('Contact'))
-        end
+    describe 'contact' do
+      before { visit contact_path }
+      it 'returns status 200' do
+        expect(page).to have_http_status '200'
       end
-
-      describe 'About' do
-        before { visit about_path }
-        it 'returns status 200' do
-          expect(page).to have_http_status '200'
-        end
-        it 'has proper title' do
-          expect(page).to have_title(full_title('About'))
-        end
+      it 'has proper title' do
+        expect(page).to have_title(full_title('Contact'))
       end
+    end
 
-      describe 'Help' do
-        before { visit help_path }
-        it 'returns status 200' do
-          expect(page).to have_http_status '200'
-        end
-        it 'has proper title' do
-          expect(page).to have_title(full_title('Help'))
-        end
+    describe 'about' do
+      before { visit about_path }
+      it 'returns status 200' do
+        expect(page).to have_http_status '200'
+      end
+      it 'has proper title' do
+        expect(page).to have_title(full_title('About'))
+      end
+    end
+
+    describe 'help' do
+      before { visit help_path }
+      it 'returns status 200' do
+        expect(page).to have_http_status '200'
+      end
+      it 'has proper title' do
+        expect(page).to have_title(full_title('Help'))
+      end
+    end
+
+    describe 'signup' do
+      before { visit signup_path }
+      it 'returns 200' do
+        expect(page).to have_http_status '200'
+      end
+      it 'has proper title' do
+        expect(page).to have_title(full_title('Sign up'))
       end
     end
   end
--- spec/requests/site_navigation_spec.rb
+++ spec/requests/site_navigation_spec.rb
@@ -2,37 +2,49 @@ require 'rails_helper'
 
 describe 'site navigation', type: :request do
   describe 'root' do
+    before { get root_path }
     it 'has value success' do
-      expect(get: root_url).to redirect_to('static_pages#home')
       expect(response).to be_success
+    end
+
+    it 'renders the home template' do
+      expect(response).to render_template(:home)
     end
   end
 
   describe 'home' do
     it 'has value success' do
-      get :home
+      get home_path
       expect(response).to be_success
     end
   end
 
   describe 'help' do
     it 'has value success' do
-      get :help
+      get help_path
       expect(response).to be_success
     end
   end
 
   describe 'about' do
     it 'has value success' do
-      get :about
+      get about_path
       expect(response).to be_success
     end
   end
 
   describe 'contact' do
     it 'has value success' do
-      get :contact
+      get contact_path
       expect(response).to be_success
     end
   end
+
+  describe 'users#new' do
+    it 'has value success' do
+      get signup_path
+      expect(response).to be_success
+    end
+  end
+
 end
